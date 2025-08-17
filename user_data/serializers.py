7060c9b75d6c63b4009314d78b400852e0a7d14e
harael93 <lvx213llc@gmail.com>
2025-08-17 12:37:27 +0000

    readings sent to book of shadows

--- user_data/serializers.py
+++ user_data/serializers.py
@@ -2,6 +2,19 @@ from rest_framework import serializers
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    def update(self, instance, validated_data):
+        # Remove confirm_password if present
+        validated_data.pop('confirm_password', None)
+        # Update all fields except profile_image
+        for attr, value in validated_data.items():
+            if attr != 'profile_image':
+                setattr(instance, attr, value)
+        # Handle profile_image separately
+        profile_image = validated_data.get('profile_image', None)
+        if profile_image:
+            instance.profile_image = profile_image
+        instance.save()
+        return instance
     class Meta:
         model = User
         fields = ['id', 'username', 'email', 'password', 'confirm_password', 'profile_image', 'bio', 'birthdate', 'birth_time']
@@ -15,8 +28,11 @@ class UserSerializer(serializers.ModelSerializer):
         }
 
     def validate(self, data):
-        if data['password'] != data['confirm_password']:
-            raise serializers.ValidationError('Passwords do not match.')
+        password = data.get('password')
+        confirm_password = data.get('confirm_password')
+        if password is not None or confirm_password is not None:
+            if password != confirm_password:
+                raise serializers.ValidationError('Passwords do not match.')
         return data
 
     def create(self, validated_data):
